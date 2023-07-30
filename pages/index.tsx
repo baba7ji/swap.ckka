e83930dea306b76e3ba4108d4e221e2a80fbda22
baba7ji <72732967+baba7ji@users.noreply.github.com>
2023-07-31 03:44:43 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
   return (
     <div style={{ height: '100vh' }}>
-      <h1>Welcome to My Home Page!</h1>
+      <h1 style={{ fontWeight: 'bold', color: 'white', textShadow: '0 0 5px rgba(255, 255, 255, 0.8)' }}>
+        Welcome to My Home Page!
+      </h1>
       <iframe
         style={{ height: '100%', width: '100%' }}
         id="dexscreener-embed"
