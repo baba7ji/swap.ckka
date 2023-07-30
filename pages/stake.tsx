d14fd4ac8ed01dd5317f1a98bf6aad897cbe2f04
baba7ji <72732967+baba7ji@users.noreply.github.com>
2023-07-31 04:20:01 +0530

    Update stake.tsx

--- pages/stake.tsx
+++ pages/stake.tsx
@@ -12,8 +12,37 @@ interface FlippedCard {
   randomNumber: number;
 }
 
-const Stake: React.FC = () => {
-  const [flippedCards, setFlippedCards] = useState<FlippedCard[]>([]);
+const flippedCardIndex = prevCards.findIndex((card) => card.index === index);
+      if (flippedCardIndex !== -1) {
+        const newCards = [...prevCards];
+        newCards.splice(flippedCardIndex, 1);
+        return newCards;
+      } else {
+        const newCard: FlippedCard = {
+          index: index,
+          randomNumber: generateRandomNumber(),
+        };
+        return [...prevCards, newCard];
+      }
+    });
+  };
+
+  const generateRandomNumber = (): number => {
+    const randomNumber = Math.random();
+    if (randomNumber < 0.6) {
+      return Math.floor(Math.random() * 10) + 1; // Generate a random number between 1 and 10 (60% probability)
+    } else if (randomNumber < 0.8) {
+      return Math.floor(Math.random() * 20) + 11; // Generate a random number between 11 and 30 (20% probability)
+    } else if (randomNumber < 0.9) {
+      return Math.floor(Math.random() * 40) + 31; // Generate a random number between 31 and 70 (10% probability)
+    } else if (randomNumber < 0.94) {
+      return Math.floor(Math.random() * 20) + 71; // Generate a random number between 71 and 90 (4% probability)
+    } else if (randomNumber < 0.96) {
+      return Math.floor(Math.random() * 10) + 91; // Generate a random number between 91 and 100 (2% probability)
+    } else {
+      return 500; // Fixed number 500 (0.1% probability)
+    }
+  };
 
   // Existing flipCard and generateRandomNumber functions...
 
