1f421654ff6c04ac96b8f60d2dc1eb50ff981c83
baba7ji <72732967+baba7ji@users.noreply.github.com>
2023-07-31 03:42:40 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
-import type { NextPage } from 'next'
+import React from 'react';
 
+const Home = () => {
+  return (
+    <div style={{ height: '100vh' }}>
+      <h1>Welcome to My Home Page!</h1>
+      <iframe
+        style={{ height: '100%', width: '100%' }}
+        id="dexscreener-embed"
+        title="dexscreener-embed"
+        src="https://dexscreener.com/solana/DGgDfszAr6BDyeFZcQDTFXDMGZicfRiXEcAVKBQ1jjV7?embed=1&theme=dark"
+        frameBorder="0"
+        allow="clipboard-write"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
 
-
-const Home: NextPage = () => {
-
-  return <>
-  </>
-}
-
-export default Home
+export default Home;
